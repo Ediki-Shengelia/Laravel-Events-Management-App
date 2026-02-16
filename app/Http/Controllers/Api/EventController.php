ad8f6c9b774ff91c515cfa3b1d5ca81dbf7321c4
Ediki Shengelia <shengelia301@gmail.com>
2026-02-16 15:39:56 +0400

    First commit

---
+++ app/Http/Controllers/Api/EventController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
+use App\Models\Event;
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return EventResource::collection(
+            Event::with('user')->paginate()
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'start_time' => 'required|date',
+            'end_time' => 'required|date|after:start_time'
+        ]);
+        $data['user_id'] = 1;
+        $event = \App\Models\Event::create($data);
+        return $event;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(\App\Models\Event $event)
+    {
+        $event->load(['user', 'attendees']);
+        return new EventResource($event);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Event $event)
+    {
+        $data = $request->validate([
+            'name' => 'sometimes|string|max:255',
+            'description' => 'nullable|string',
+            'start_time' => 'sometimes|date',
+            'end_time' => 'sometimes|date|after:start_time'
+        ]);
+        $event->update($data);
+        return $event;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Event $event)
+    {
+        $event->delete();
+        return response()->json([
+            "message" => "Event deleted Successfully"
+        ]);
+    }
+}
